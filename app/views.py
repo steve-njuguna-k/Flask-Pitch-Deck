e7daeb26db6782983aa7dccabf0081c95542f795
Steve Njuguna <stevenjuguna93@gmail.com>
2022-01-05 17:19:47 +0300

    Added Login & Register page

--- app/views.py
+++ app/views.py
@@ -4,3 +4,11 @@ from flask import render_template
 @app.route('/')
 def home():
     return render_template('Index.html')
+
+@app.route('/login')
+def login():
+    return render_template('Login.html')
+
+@app.route('/register')
+def register():
+    return render_template('Register.html')
