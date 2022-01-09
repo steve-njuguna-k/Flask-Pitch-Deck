7d7306c32ee62e33d876fd16224901663b2a623e
Steve Njuguna <stevenjuguna93@gmail.com>
2022-01-09 21:22:06 +0300

    Added add pitch function in views.py

--- app/views.py
+++ app/views.py
@@ -2,17 +2,29 @@ import datetime
 from app import app
 from flask import render_template, flash, redirect, request, url_for
 from .email import send_email
-from .models import UserModel, db
+from .models import UserModel, db, Pitch
 from flask_login import current_user, login_user, logout_user, login_required
 from .forms import LoginForm, RegisterForm, PitchForm
 from flask_bcrypt import Bcrypt
 from .token import confirm_token, generate_confirmation_token
 bcrypt = Bcrypt(app)
 
-@app.route('/')
+@app.route('/', methods=['POST','GET'])
 def home():
+    pitches = Pitch.query.all()
     form = PitchForm()
-    return render_template('Index.html', form = form)
+    if form.validate_on_submit():
+        pitch_body = form.pitch_body.data
+        category = form.category.data
+        user_id = current_user._get_current_object().id
+
+        pitch_obj = Pitch(user_id = user_id, pitch_body = pitch_body, category = category)
+        db.session.add(pitch_obj)
+        db.session.commit()
+        
+        return redirect(url_for('home'))
+
+    return render_template('Index.html', form = form, pitches = pitches)
 
 @app.route('/login/', methods = ['GET', 'POST'])
 def login():
