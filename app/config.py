d7605caa04c7f038796b664ed159f7f51a95294b
Steve Njuguna <stevenjuguna93@gmail.com>
2022-01-13 19:41:01 +0300

    Added logic to DB URL

--- app/config.py
+++ app/config.py
@@ -3,7 +3,9 @@ import os
 # main config
 SECRET_KEY = os.environ.get('SECRET_KEY')
 SECURITY_PASSWORD_SALT = os.environ.get('SECURITY_PASSWORD_SALT')
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") 
+if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
 SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS')
 DEBUG = False
 BCRYPT_LOG_ROUNDS = 13
