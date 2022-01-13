8296d566f88bcc9449f9e68298aa0aa836094f73
Steve Njuguna <stevenjuguna93@gmail.com>
2022-01-13 22:39:15 +0300

    Added Comment Test

---
+++ tests/comment_test.py
@@ -0,0 +1,12 @@
+import unittest
+from app.models import Comments
+
+class CommentsTest(unittest.TestCase):
+    def setUp(self):
+        self.new_comment = Comments(
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", 
+            "2021-12-13T20:19:00Z", 
+        )
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_user, Comments))
